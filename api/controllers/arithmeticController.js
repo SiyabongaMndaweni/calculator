d23fedb3ae002fb11e4596225d132151260d61b3
SiyabongaMndaweni <82041335+SiyabongaMndaweni@users.noreply.github.com>
2022-07-29 10:41:13 +0200

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.calculate = function(req, res) {
+exportscalculate = function(req, res) {
   req.app.use(function(err, req, res, next) {
     if (res.headersSent) {
       return next(err);
@@ -47,4 +47,4 @@ exports.calculate = function(req, res) {
   var operand2 = parseInt(req.query.operand2, 10);
 
   res.json({ result: operation(req.query.operand1, req.query.operand2) });
-};
+
